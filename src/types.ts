6436e665238dfec3b0b5e7e98a5324e4ba1057f7
Adam Bradley <adamdbradley@users.noreply.github.com>
2020-05-16 01:37:02 -0500

    🐞

--- src/types.ts
+++ src/types.ts
@@ -1,17 +1,27 @@
-export interface ReactiveForm extends ReactiveFormOptions {}
-
-export interface ReactiveFormOptions {
-  id: string;
-}
-
-export type ReactiveFormValidateResults = string | undefined | null;
-
-export interface ReactiveSubmit {}
-
 export interface ReactiveFormBindOptions {
+  /**
+   * The JSX event name property used to listen for input changes.
+   * Defaults to `onInput` for text inputs, and `onChange` for
+   * `checkbox` and `radio`. This can be used to bind to custom
+   * event names, such as `ionOnChange`.
+   */
   changeEventName?: string;
+  /**
+   * Number of milliseconds to debounce the value change event.
+   * By default there is no debounce.
+   */
   debounce?: number;
+  /**
+   * The "id" attribute/property to use for the input element.
+   * By default the id is created from the property name passed
+   * into the bind function.
+   */
   id?: string;
+  /**
+   * The "name" attribute/property to use for the input element.
+   * By default the name is created from the property name passed
+   * into the bind function.
+   */
   name?: string;
   onBlur?: (value: any, validity: ValidityState, ev: FocusEvent) => void;
   onEnterKey?: (value: any, validity: ValidityState, ev: KeyboardEvent) => void;
@@ -26,9 +36,23 @@ export interface ReactiveFormControlOptions extends ReactiveFormBindOptions {
   onValueChange?: (value: any, validity: ValidityState, ev: UIEvent) => void;
 }
 
+export interface ReactiveForm extends ReactiveFormOptions {}
+
+export interface ReactiveFormOptions {
+  id: string;
+}
+
+export type ReactiveFormValidateResults = string | undefined | null;
+
+export interface ReactiveSubmit {}
+
+export type ReactiveFormControl = () => ReactiveControlProperties;
+
+export type ReactiveFormControlGroup = (groupItemId?: string, groupItemValue?: string) => ReactiveControlProperties;
+
+/** @internal */
 export type ReactiveControlProperties = {
   checked?: boolean;
-  htmlForm?: string;
   id: string;
   name?: string;
   onBlur?: (ev: FocusEvent) => void;
@@ -41,10 +65,6 @@ export type ReactiveControlProperties = {
   role?: string;
   value?: string;
 };
-
-export type ReactiveFormControl = () => ReactiveControlProperties;
-
-export type ReactiveFormControlGroup = (groupItemId?: string, groupItemValue?: string) => ReactiveControlProperties;
 
 /** @internal */
 export type ControlElement = HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement;
