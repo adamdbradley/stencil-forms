8948a63fb06694b5842194c446ff9b155ae6d950
Manu MA <manu.valladolid@gmail.com>
2020-08-25 17:41:06 +0200

    Update validation.ts

--- src/validation.ts
+++ src/validation.ts
@@ -190,8 +190,10 @@ export const isTouched = (ctrl: ReactiveFormControl) => !!getControlState(ctrl).
 
 export const submitValidity = (message: string | undefined) => {
   return {
-    ref(btn: HTMLInputElement | HTMLButtonElement) {
-      btn.setCustomValidity(message ?? '');
+    ref(btn: HTMLInputElement | HTMLButtonElement | undefined) {
+      if (btn) {
+        btn.setCustomValidity(message ?? '');
+      }
     },
   };
 };
